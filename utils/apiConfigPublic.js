12e2c5d6c89a60e494e69f233875338db2d406ba
Aditya Rathore <145015316+01AdI@users.noreply.github.com>
2026-09-08 23:37:37 +0530

    Update apiConfigPublic.js

--- utils/apiConfigPublic.js
+++ utils/apiConfigPublic.js
@@ -1,4 +1,4 @@
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://times-india-travels-next-frontent.onrender.com/";
 
 export default API_BASE_URL;
